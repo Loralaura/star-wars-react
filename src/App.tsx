9be0920e08dcab225f1de7534451538fe5f1792e
Laura Robinson <laura3918@hotmail.co.uk>
2023-12-18 20:21:22 +0000

    feat: Initialising repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ApiResult from "./components/api_result";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*         <section className="boilerplate">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section> */}
+        <ApiResult />
       </header>
     </div>
   );
---
+++ src/components/api_result.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ApiResult() {
+  return <h1>Example</h1>;
+}
+
+export default ApiResult;
